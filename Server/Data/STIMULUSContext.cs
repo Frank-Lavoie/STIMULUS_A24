345d40b2a821a0855417ae759e5775963832054f
ChuckD46 <charles_d46@outlook.com>
2023-10-28 14:50:55 -0400

    SetUp Core Application Structure : File Structure - EF Core - Radzen - Swagger - Serilog

---
+++ Server/Data/STIMULUSContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace STIMULUS_V2.Server.Data
+{
+    public class STIMULUSContext : DbContext
+    {
+        public STIMULUSContext(DbContextOptions<STIMULUSContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+using STIMULUS_V2.Server.Data;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddSwaggerGen();
+
+builder.Host.UseSerilog((ctx, lc) =>
+    lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
+
+builder.Services.AddDbContextPool<STIMULUSContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("STIMULUSConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
