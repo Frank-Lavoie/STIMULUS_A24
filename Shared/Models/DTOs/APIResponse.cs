01c7c65a0f5773b1eb78d1376e60eb2070a58808
ChuckD46 <charles_d46@outlook.com>
2023-10-28 14:57:53 -0400

    Adding APIResponse DTOs to the Application

---
+++ Shared/Models/DTOs/APIResponse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STIMULUS_V2.Shared.Models.DTOs
+{
+    public class APIResponse<T>
+    {
+        public T? Data { get; set; }
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+
+        public APIResponse()
+        {
+            Data = default(T);
+            StatusCode = 200;
+            Message = string.Empty;
+        }
+
+        public APIResponse(T data, int statusCode, string message)
+        {
+            Data = data;
+            StatusCode = statusCode;
+            Message = message;
+        }
+    }
+}
