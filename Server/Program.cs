13bd9d5942a9277067e0e5f4358b3a1c902e1d1f
Marc-Antoine-B <77787920+Marc-Antoine-B@users.noreply.github.com>
2023-12-08 18:41:27 -0500

    177 add graphe instance newetudiant (#178)
    * fix 404 quand on lance l'appli
    
    * test
    
    * add sutdent in groupe
    
    * prêt

--- Server/Program.cs
+++ Server/Program.cs
@@ -71,7 +71,7 @@ using (var scope = app.Services.CreateScope())
     context.EnsureEtuUserCreated();
     context.EnsureProfUserCreated();
 
-    //Création du trigger pour changer le status
+    ////Création du trigger pour changer le status de bloqué/débloqué
     //context.Database.ExecuteSqlRaw(@"
     //    CREATE TRIGGER TRG_UpdateStatus
     //    ON Noeud
