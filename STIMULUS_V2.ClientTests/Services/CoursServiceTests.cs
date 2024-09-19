86198c51abfc7b7660606174f2b4d6f779a06c7e
Frank-Lavoie <francis.lavoie79@outlook.com>
2024-09-19 13:57:43 -0400

    Ajout des tests unitaires.

---
+++ STIMULUS_V2.ClientTests/Services/CoursServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using STIMULUS_V2.Client.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STIMULUS_V2.Client.Services.Tests
+{
+    [TestClass()]
+    public class CoursServiceTests
+    {
+        [TestMethod()]
+        public void CreateTest()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
